35e9074299e0510a0bd8752f8e2938f57d85dd49
grsagor <grsagor08@gmail.com>
2022-11-11 20:01:02 +0600

    review displayed

--- src/Pages/Review/Review.js
+++ src/Pages/Review/Review.js
@@ -1,9 +1,20 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
+import SingleReview from './SingleReview/SingleReview';
 
 const Review = ({service}) => {
+    const [reviews, setReviews] = useState([]);
     const {_id, title} = service;
     const {user} = useContext(AuthContext);
+
+    const url = `http://localhost:5000/reviews?id=${_id}`
+    useEffect( () => {
+        fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            setReviews(data)
+        })
+    },[])
 
     const handleReview = event => {
         event.preventDefault();
@@ -17,6 +28,23 @@ const Review = ({service}) => {
             userName: name,
             review: review,
         }
+
+        fetch('http://localhost:5000/reviews', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(reviews)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data.acknowledged){
+                alert('Review submitted successfully');
+                form.reset();
+            }
+        })
+        .catch(err => console.error(err));
     }
     return (
         <div>
@@ -24,6 +52,13 @@ const Review = ({service}) => {
                 <textarea className='textarea textarea-bordered h-24 w-full' type="text" name="review" placeholder='Enter Your Review' id="" />
                 <input className='btn' type="submit" value="Submit Your Review" />
             </form>
+            <div>
+                {
+                    reviews.map( review => <SingleReview
+                        review = {review}
+                    ></SingleReview>)
+                }
+            </div>
         </div>
     );
 };
---
+++ src/Pages/Review/SingleReview/SingleReview.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SingleReview = ({review}) => {
+    return (
+        <div>
+            <h2>{review.review}</h2>
+        </div>
+    );
+};
+
+export default SingleReview;
